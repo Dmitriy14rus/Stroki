5d3dbf05c4b4fa2faf489c984341a0b721d9e94e
жданкин <@DESKTOP-G68KNF7>
2023-05-06 18:29:45 +0300

    Добавьте файлы проекта.

---
+++ дз3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace дз3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         string name;
+         int age;
+         string zodiacSign;
+         string job;
+
+         Console.Write("Введите ваше Имя: ");
+         name = Console.ReadLine();
+         Console.Clear();
+         Console.Write("Введите ваш возвраст: ");
+         age = Convert.ToInt32(Console.ReadLine());
+         Console.Clear();
+         Console.Write("Введите ваш знак зодиака: ");
+         zodiacSign = Console.ReadLine();
+         Console.Clear();
+         Console.Write("Введите кем вы работаете: ");
+         job = Console.ReadLine();
+         Console.Clear();
+         Console.WriteLine($"Вас зовут {name},вам {age} лет!Ваш знак зодиака {zodiacSign}.Ваша должность {job}.");
+         Console.ReadKey();
+        }
+
+
+        
+    }
+}
